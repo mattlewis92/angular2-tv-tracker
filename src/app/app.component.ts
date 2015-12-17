52c8bd37f677d333e4f8acdea9df7153070436f0
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-12-17 15:51:53 +0000

    Bootstrap angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'tv-tracker',
+    template: '<h1>Hello world from the TV tracker</h1>'
+})
+export class AppComponent { }
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: './src/entry.ts',
+  output: {
+    filename: 'tv-tracker.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.ts$/, loader: 'ts', exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+  devServer: {
+    port: 8000,
+    inline: true,
+    hot: true,
+    historyApiFallback: true,
+    contentBase: 'src/public'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
