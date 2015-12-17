12cabfbbab0491fb2d957fd506bcc2dc29b59899
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-12-17 16:05:40 +0000

    Fix formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'tv-tracker',
-    template: '<h1>Hello world from the TV tracker</h1>'
+  selector: 'tv-tracker',
+  template: `
+    <h1>Hello world from the TV tracker</h1>
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+}
