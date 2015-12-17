8ff21c173b1150f3560582bc438a65011dc59182
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-12-17 17:55:26 +0000

    Drop the component suffix from the app component

--- src/entry.ts
+++ src/entry.ts
@@ -7,6 +7,6 @@ import 'bootstrap/scss/bootstrap.scss';
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {TVSearch} from './app/services/TVSearch';
-import {AppComponent} from './app/app.component';
+import {AppComponent} from './app/app';
 
 bootstrap(AppComponent, [TVSearch, HTTP_PROVIDERS]);
