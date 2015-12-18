ce12a5efba893e8bdffef2af5c7a9630eed04f1a
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-12-18 17:18:31 +0000

    Refactor the search box to use a form

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Angular2 TV tracker
 
 TODOS
-* Additional pages using the router
-* Some kind of form
 * Unit tests
--- src/app/components/showSearch.ts
+++ src/app/components/showSearch.ts
@@ -1,16 +1,26 @@
 import {Component} from 'angular2/core';
+import {FormBuilder, Validators, ControlGroup, FORM_DIRECTIVES} from 'angular2/common';
 import {TVMaze} from './../services/tvMaze';
-import {SearchBox} from './searchBox';
 import {ShowList} from './showList';
 
 @Component({
   selector: 'tv-tracker',
-  directives: [[SearchBox], [ShowList]], // make webstorm happy :/
+  directives: [[ShowList], FORM_DIRECTIVES], // make webstorm happy
   providers: [TVMaze],
   template: `
     <h1>Search for a show</h1>
-    <form (submit)="searchShows(searchBox.searchText)">
-      <search-box #searchBox></search-box>
+    <form (submit)="$event.preventDefault(); searchShows(searchForm.value.query)" [ngFormModel]="searchForm">
+      <div class="input-group">
+        <input type="search" class="form-control" ngControl="query">
+        <span class="input-group-btn">
+          <button
+            class="btn btn-primary"
+            type="submit"
+            [disabled]="!searchForm.valid">
+            Search
+           </button>
+        </span>
+      </div>
     </form>
     <br>
     <show-list [shows]="shows"></show-list>
@@ -25,8 +35,13 @@ export class ShowSearch {
 
   public shows:Array<Object>;
   public error:string;
+  private searchForm:ControlGroup;
 
-  constructor(private tvMaze:TVMaze) {}
+  constructor(private tvMaze:TVMaze, fb:FormBuilder) {
+    this.searchForm = fb.group({
+      query: ['', Validators.required]
+    });
+  }
 
   resetSearch() {
     this.error = null;
