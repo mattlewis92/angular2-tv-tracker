c1381e745f152b0dc9f2cf3931cb7f5dc435f1c7
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-02-23 14:48:15 +0000

    Use typescript 1.8s allowJs option and remove the need for the whole of lodash to be installed

--- src/app/pipes/orderBy.ts
+++ src/app/pipes/orderBy.ts
@@ -1,5 +1,7 @@
 import {Pipe, PipeTransform} from 'angular2/core';
-import * as _ from 'lodash';
+
+declare var require: any;
+const get: any = require('lodash.get');
 
 @Pipe({
   name: 'orderBy'
@@ -9,10 +11,10 @@ export class OrderBy implements PipeTransform {
   transform(input: Object[], [field, desc = false]: [string, boolean]): Object[] {
     if (input && field) {
       return Array.from(input).sort((a: Object, b: Object) => {
-        if (_.get(a, field) < _.get(b, field)) {
+        if (get(a, field) < get(b, field)) {
           return desc ? 1 : -1;
         }
-        if (_.get(a, field) > _.get(b, field)) {
+        if (get(a, field) > get(b, field)) {
           return desc ? -1 : 1;
         }
         return 0;
