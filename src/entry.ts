c04e079c37e965bffa8a83f910af8d4edbf0eae9
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-03-19 09:18:43 +0000

    Upgrade to angular beta 11

--- src/entry.ts
+++ src/entry.ts
@@ -3,7 +3,7 @@
 import 'es6-shim';
 import 'es7-shim';
 import 'reflect-metadata';
-import 'zone.js/dist/zone-microtask';
+import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'rxjs';
 import 'bootstrap/scss/bootstrap.scss';
