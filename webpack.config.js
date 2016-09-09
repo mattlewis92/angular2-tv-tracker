4ba3e76f85ad323390c5be6e5a730a28b41468eb
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-09-09 13:07:27 +0100

    Upgrade to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,47 @@
-require('core-js');
 const webpack = require('webpack');
-const IS_PROD = process.argv.indexOf('--production') > -1;
 
-module.exports = {
-  devtool: IS_PROD ? 'source-map' : 'eval',
-  entry: './src/entry.ts',
-  output: {
-    filename: 'tv-tracker.js'
-  },
-  module: {
-    preLoaders: [{
-      test: /\.ts$/, loader: 'tslint?emitErrors=false&failOnHint=false', exclude: /node_modules/
-    }],
-    loaders: [{
-      test: /\.ts$/, loader: 'ts', exclude: /node_modules/
-    }, {
-      test: /\.scss$/, loader: 'style!css!sass'
-    }, {
-      test: /\.css$/, loader: 'style!css'
-    }]
-  },
-  resolve: {
-    extensions: ['', '.ts', '.js']
-  },
-  devServer: {
-    port: 8000,
-    inline: true,
-    hot: true,
-    historyApiFallback: true,
-    contentBase: 'src/public'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.DefinePlugin({
-      ENV: JSON.stringify(IS_PROD ? 'production' : 'development')
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: false,
-      compress: {
-        warnings: false
-      }
-    })
-  ]
+module.exports = env => {
+
+  return {
+    devtool: env.production ? 'source-map' : 'eval',
+    entry: './src/entry.ts',
+    output: {
+      filename: 'tv-tracker.js'
+    },
+    module: {
+      preLoaders: [{
+        test: /\.ts$/, loader: 'tslint?emitErrors=false&failOnHint=false', exclude: /node_modules/
+      }],
+      loaders: [{
+        test: /\.ts$/, loader: 'ts', exclude: /node_modules/
+      }, {
+        test: /\.scss$/, loader: 'style!css!sass'
+      }, {
+        test: /\.css$/, loader: 'style!css'
+      }]
+    },
+    resolve: {
+      extensions: ['', '.ts', '.js']
+    },
+    devServer: {
+      port: 8000,
+      inline: true,
+      hot: true,
+      historyApiFallback: true,
+      contentBase: 'src/public'
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        ENV: JSON.stringify(env.production ? 'production' : 'development')
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        mangle: false,
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  };
+
 };
