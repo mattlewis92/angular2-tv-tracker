6dc1f8107e8e2a3148094f3e8b54758f7f2963c0
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-10-13 18:32:55 +0100

    Use es6 import for service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule, PreloadAllModules} from '@angular/router';
 import {Loading, LoadingSpinner} from './modules/loading';
 import {AppComponent} from './app.component';
+import {install} from 'offline-plugin/runtime';
 
-require('offline-plugin/runtime').install();
+install();
 
 declare var ENV: string;
 if (ENV === 'production') {
