b008568d8df53c98223de17d0f2b57feadb31e39
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-11-04 15:11:19 +0000

    Upgrade calendar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@
 
 import 'bootstrap/scss/bootstrap.scss';
 import 'font-awesome/scss/font-awesome.scss';
-import 'angular2-calendar/scss/angular2-calendar.scss';
+import 'angular-calendar/scss/angular-calendar.scss';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 import 'rxjs/Observable';
