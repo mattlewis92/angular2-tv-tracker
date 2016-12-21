f54fa4f5d21709b8ae5b7e58e18bfd299c1fed4d
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-12-21 11:53:08 +0000

    Use webpack config utils

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,18 +4,21 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const FixDefaultImportPlugin = require('webpack-fix-default-import-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OfflinePlugin = require('offline-plugin');
+const {getIfUtils, removeEmpty} = require('webpack-config-utils');
 
 module.exports = env => {
 
-  const outputFilename = env.production ? '[name]-[chunkhash]' : '[name]';
+  const environment = getIfUtils(env.production ? 'production': 'development');
+  const {ifProduction} = getIfUtils(environment);
+  const outputFilename = ifProduction('[name]-[chunkhash]', '[name]');
   const extractCSS = new ExtractTextPlugin(`${outputFilename}.css`);
 
   return {
-    devtool: env.production ? 'source-map' : 'eval',
-    entry: env.production ? './src/entry.aot.ts' : './src/entry.jit.ts',
+    devtool: ifProduction('source-map', 'eval'),
+    entry: ifProduction('./src/entry.aot.ts', './src/entry.jit.ts'),
     output: {
       filename: `${outputFilename}.js`,
-      publicPath: env.production ? '/angular2-tv-tracker/' : '/'
+      publicPath: ifProduction('/angular2-tv-tracker/', '/')
     },
     module: {
       rules: [{
@@ -25,7 +28,7 @@ module.exports = env => {
         enforce: 'pre'
       }, {
         test: /\.ts$/,
-        loader: 'awesome-typescript-loader!angular2-router-loader?loader=system&genDir=./aot/src/app' + (env.production ? '&aot=true' : ''),
+        loader: 'awesome-typescript-loader!angular2-router-loader?loader=system&genDir=./aot/src/app' + ifProduction('&aot=true' , ''),
         exclude: path.resolve(__dirname, 'node_modules')
       }, {
         test: /\.scss$/,
@@ -45,10 +48,10 @@ module.exports = env => {
       port: 8000,
       inline: true
     },
-    plugins: [
-      ...(env.production ? [new webpack.optimize.UglifyJsPlugin({sourceMap: true})] : []),
+    plugins: removeEmpty([
+      ifProduction(new webpack.optimize.UglifyJsPlugin({sourceMap: true})),
       new webpack.DefinePlugin({
-        ENV: JSON.stringify(env.production ? 'production' : 'development')
+        ENV: JSON.stringify(environment)
       }),
       extractCSS,
       new webpack.ContextReplacementPlugin(
@@ -65,8 +68,8 @@ module.exports = env => {
         template: 'src/index.ejs',
         title: 'Angular 2 TV tracker'
       }),
-      ...(env.production ? [new OfflinePlugin({})] : [])
-    ],
+      ifProduction(new OfflinePlugin({}))
+    ]),
     performance: {
       hints: false
     }
