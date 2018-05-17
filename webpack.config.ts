b4aac69c85005e1a75a4d80ae7378f40b1ed332b
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2018-05-17 18:25:56 +0100

    Fix prod build

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,8 +8,7 @@ import { getIfUtils, removeEmpty } from 'webpack-config-utils';
 import { AngularCompilerPlugin } from '@ngtools/webpack';
 
 export default (environment: string) => {
-
-  const {ifProduction, ifDevelopment} = getIfUtils(environment);
+  const { ifProduction, ifDevelopment } = getIfUtils(environment);
   const outputFilename = ifProduction('[name]-[chunkhash]', '[name]');
   const PROD_PUBLIC_PATH = '/angular2-tv-tracker/';
 
@@ -22,55 +21,68 @@ export default (environment: string) => {
       publicPath: ifProduction(PROD_PUBLIC_PATH, '/')
     },
     module: {
-      rules: removeEmpty([ifDevelopment({
-        test: /\.ts$/,
-        loader: 'tslint-loader',
-        exclude: /node_modules/,
-        enforce: 'pre',
-        options: {
-          emitErrors: false,
-          failOnHint: false
-        }
-      }), ifProduction({
-        test: /\.ts$/,
-        loader: '@ngtools/webpack'
-      }, {
-        test: /\.ts$/,
-        use: [{
-          loader: 'ts-loader',
+      rules: removeEmpty([
+        ifDevelopment({
+          test: /\.ts$/,
+          loader: 'tslint-loader',
+          exclude: /node_modules/,
+          enforce: 'pre',
           options: {
-            transpileOnly: true,
-            compilerOptions: {
-              module: 'es2015'
-            }
+            emitErrors: false,
+            failOnHint: false
           }
-        }, {
-          loader: 'angular-router-loader'
-        }],
-        exclude: path.resolve(__dirname, 'node_modules')
-      }), {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader?minimize',
-          'sass-loader'
-        ]
-      }, {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          mimetype: 'application/font-woff'
+        }),
+        ifProduction(
+          {
+            test: /\.ts$/,
+            loader: '@ngtools/webpack'
+          },
+          {
+            test: /\.ts$/,
+            use: [
+              {
+                loader: 'ts-loader',
+                options: {
+                  transpileOnly: true,
+                  compilerOptions: {
+                    module: 'es2015'
+                  }
+                }
+              },
+              {
+                loader: 'angular-router-loader'
+              }
+            ],
+            exclude: path.resolve(__dirname, 'node_modules')
+          }
+        ),
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader?minimize',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff'
+          }
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'file-loader'
+        },
+        {
+          test: /node_modules\/@angular\/core\/.+\/core\.js$/,
+          parser: {
+            system: true // disable `System.import() is deprecated and will be removed soon. Use import() instead.` warning
+          }
         }
-      }, {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
-      }, {
-        test: /node_modules\/@angular\/core\/.+\/core\.js$/,
-        parser: {
-          system: true // disable `System.import() is deprecated and will be removed soon. Use import() instead.` warning
-        }
-      }])
+      ])
     },
     resolve: {
       extensions: ['.ts', '.js']
@@ -81,9 +93,11 @@ export default (environment: string) => {
     },
     plugins: removeEmpty([
       ifDevelopment(new ForkTsCheckerWebpackPlugin()),
-      ifProduction(new AngularCompilerPlugin({
-        tsConfigPath: './tsconfig-aot.json'
-      })),
+      ifProduction(
+        new AngularCompilerPlugin({
+          tsConfigPath: './tsconfig-aot.json'
+        })
+      ),
       new MiniCssExtractPlugin({
         filename: `${outputFilename}.css`
       }),
@@ -93,23 +107,28 @@ export default (environment: string) => {
       ),
       new HtmlWebpackPlugin({
         template: 'src/index.ejs',
-        title: 'Angular 2+ TV tracker'
+        title: 'Angular 2+ TV tracker',
+        chunksSortMode: 'none'
       }),
-      ifProduction(new OfflinePlugin({
-        ServiceWorker: {
-          navigateFallbackURL: PROD_PUBLIC_PATH
-        }
-      }))
+      ifProduction(
+        new OfflinePlugin({
+          ServiceWorker: {
+            navigateFallbackURL: PROD_PUBLIC_PATH
+          }
+        })
+      )
     ]),
     optimization: {
-      splitChunks: ifProduction({
-        chunks: 'all',
-        automaticNameDelimiter: '.'
-      }, false),
+      splitChunks: ifProduction(
+        {
+          chunks: 'all',
+          automaticNameDelimiter: '.'
+        },
+        false
+      ),
       runtimeChunk: ifProduction(true, false),
       removeAvailableModules: ifProduction(true, false), // disable tree shaking in dev mode for faster rebuilds
       removeEmptyChunks: ifProduction(true, false)
     }
   };
-
 };
