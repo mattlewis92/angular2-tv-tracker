733f0a929c8da7e4f52739103f5ccaa30ea80158
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2018-05-17 18:50:20 +0100

    Preserve whitespaces

--- src/entry.ts
+++ src/entry.ts
@@ -2,4 +2,6 @@ import 'core-js/es7/reflect';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  preserveWhitespaces: true
+});
